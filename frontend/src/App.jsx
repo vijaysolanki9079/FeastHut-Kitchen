da4aa8c22f6d934f4dfaf64b12bb7c677c88736f
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-10-02 16:42:05 +0530

    completed Explore-Menu section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Navbar } from './componemts/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import { Cart } from './pages/Cart/Cart';
+
 
 const App = () => {
-  return (
-    <div className='app'>
-      <Navbar />
-    </div>
 
+  return (
+    <div className='app'> 
+      <Navbar />
+      <Routes>
+          <Route path='/' element = {<Home />} />
+          <Route path='/cart' element = {<Cart />} />
+          <Route path='/placeorder' element = {<PlaceOrder />} />
+      </Routes>
+    </div>
   );
+  
 };
 
 export default App;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
+import { Header } from '../../componemts/Header/Header';
+import ExploreMenu from '../../componemts/ExploreMenu/ExploreMenu';
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
-    <div>Home</div>
-  )
+    <div>
+        <Header />
+        <ExploreMenu category= {category} setCategory={setCategory} />
+    </div>
+  );
 }
 
 export default Home
