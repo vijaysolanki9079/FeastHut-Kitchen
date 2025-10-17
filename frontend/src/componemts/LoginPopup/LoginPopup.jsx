79b4cbcfd2d1cc3d926f79c4bca34b6aa077fba2
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-10-17 22:19:10 +0530

    sign-in-page-done

--- frontend/src/componemts/LoginPopup/LoginPopup.jsx
+++ frontend/src/componemts/LoginPopup/LoginPopup.jsx
@@ -15,12 +15,22 @@ const LoginPopup = ({setShowLogin}) => {
             </div>
 
             <div class="login-popup-inputs">
-              <input type="text" placeholder='Your Name' requierd />
+              {/* At Login Time Just email and pass fields req. */}
+              {currState === "Login" ? <></> : <input type="text" placeholder='Your Name' requierd />}
               <input type="email" placeholder='Your Email' required />
               <input type="password" placeholder='Password' required />
             </div>
 
             <button>{currState === "Sign Up" ? "Create Account" : "Login"}</button>
+            <div class="login-popup-condition">
+              <input type="checkbox" required />
+              <p>By continuing, I agree to the terms of use & privacy policy.</p>
+            </div>
+            {currState==="Login" ? 
+              <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
+              : <p>Already have a account? <span onClick={() => setCurrState("Login")}>Login here</span> </p>
+            }
+            
         </form>
     </div>
   )
