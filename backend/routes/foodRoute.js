e09c5bd541ad81db2c3c0c2ce81197ccb4bdbe00
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-10-19 23:43:02 +0530

    Added-Listfood-API-&-RemoveFood-API

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFood, listFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 // 1. Add newfood 
@@ -17,9 +17,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }); 
 foodRouter.post("/add", upload.single("image"), addFood);
 
-//2. 
+// 2. 
 foodRouter.get("/list", listFood)
 
+// 3. 
+foodRouter.post("/remove", removeFood);
 
 
 export default foodRouter;
