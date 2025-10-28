3c5a42eb165a806e421d31c396486413b48a3461
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-10-28 23:57:41 +0530

    today-work-done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 
@@ -15,10 +12,9 @@ function App() {
       <div class="app-content">
         <Sidebar />
       </div>
-
     </div>
       
-  )
+  );
 }
 
 export default App
