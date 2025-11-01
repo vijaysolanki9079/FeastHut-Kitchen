53771d7686cc90145c3e644073e8b4d136a5b71c
Vijay Solanki <vijaysolanki9079@gmail.com>
2025-11-01 13:36:32 +0530

    initiating-frontend-for-sign-in-popup

--- frontend/src/componemts/LoginPopup/LoginPopup.jsx
+++ frontend/src/componemts/LoginPopup/LoginPopup.jsx
@@ -5,6 +5,7 @@ import { assets } from '../../assets/assets'
 const LoginPopup = ({setShowLogin}) => {
 
   const [currState, setCurrState] = useState("Sign Up")
+  
 
   return (
     <div className='login-popup'>
