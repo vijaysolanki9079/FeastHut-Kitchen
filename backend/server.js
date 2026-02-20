e850e69d3b9327607c3107f4328457b0b1045a75
Vijay Solanki <vijaysolanki9079@gmail.com>
2026-02-20 15:21:10 +0530

    fix render start script

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+// const port = 4000;
+const port = process.env.PORT || 10000;
 
 // middleware
 app.use(express.json());
